f3eb0bd8e2988f0d0db0ec30549c14eee2dad6f7
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2025-04-09 11:15:46 +0700

    feat: Remove Unused Test File

--- tests/Feature/Livewire/Admin/Exchanges/ExchangeListTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\Admin\Exchanges\ExchangeList;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(ExchangeList::class)
-        ->assertStatus(200);
-});
--- tests/Feature/Livewire/Admin/Users/UserListTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\Admin\Users\UserList;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(UserList::class)
-        ->assertStatus(200);
-});
